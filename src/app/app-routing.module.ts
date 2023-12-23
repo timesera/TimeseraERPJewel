bdcf0f1ea5edffd4275ae604484e32e2f599ce48
timesera <timesera9090@gmail.com>
2023-12-23 15:23:30 +0530

    Added login and erp components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TimeseraloginComponent } from './timeseralogin/timeseralogin.component';
+import { ErpComponent } from './erp/erp.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: TimeseraloginComponent
+  },
+  {
+    path: 'erp', component: ErpComponent,
+    children: [
+      {
+        path: '', component: DashboardComponent
+      },
+      {
+        path: '**', component: DashboardComponent
+      }
+    ]
+  },
+  {
+    path: '**', component: TimeseraloginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/erp/erp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-erp',
+  templateUrl: './erp.component.html',
+  styleUrls: ['./erp.component.css']
+})
+export class ErpComponent {
+
+}
