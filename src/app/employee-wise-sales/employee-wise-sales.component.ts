54698282d438c8d3af0f2b9aecba46cfb2f1e2ca
manasa1230 <91188797+manasa1230@users.noreply.github.com>
2023-12-30 12:28:15 +0530

    added Employee-wise-sales

--- src/app/employee-wise-sales/employee-wise-sales.component.ts
+++ src/app/employee-wise-sales/employee-wise-sales.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ErpService } from '../erp.service';
 
 @Component({
   selector: 'app-employee-wise-sales',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-wise-sales.component.css']
 })
 export class EmployeeWiseSalesComponent {
+  displayedColumns: string[] = ['position','empCode','pcs','gwt','nwt','amount','cmsn'];
 
+  empWiseSaleStrtDt:Date=new Date();
+  empWiseSaleEndDt:Date=new Date();
+  empCode:any;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private service: ErpService){
+  }
 }
