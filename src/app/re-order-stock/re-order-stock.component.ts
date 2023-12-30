cf1bd8bceb14d8f9456700ac6ae2a85d6db3617d
manasa1230 <91188797+manasa1230@users.noreply.github.com>
2023-12-30 14:09:28 +0530

    added ReOrder-Stock

--- src/app/re-order-stock/re-order-stock.component.ts
+++ src/app/re-order-stock/re-order-stock.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ErpService } from '../erp.service';
 
 @Component({
   selector: 'app-re-order-stock',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./re-order-stock.component.css']
 })
 export class ReOrderStockComponent {
+  displayedColumns: string[] = ['position','prodCatgry','prodName','minQty','pcs','balPcs','excesPcs'];
 
+  
+  prodList:any=[];
+  prodName:any;
+  catList:any=[];
+  catName:any;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private service: ErpService){
+  }
 }
