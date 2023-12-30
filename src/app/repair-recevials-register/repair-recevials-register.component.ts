2a6f6dc1dd36483a7f39d7dfb74ca38d15084ba2
manasa1230 <91188797+manasa1230@users.noreply.github.com>
2023-12-30 13:15:06 +0530

    added Repair-Recevials Register

--- src/app/repair-recevials-register/repair-recevials-register.component.ts
+++ src/app/repair-recevials-register/repair-recevials-register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ErpService } from '../erp.service';
 
 @Component({
   selector: 'app-repair-recevials-register',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./repair-recevials-register.component.css']
 })
 export class RepairRecevialsRegisterComponent {
+  displayedColumns: string[] = ['position','reprNo','date','custName','mobileNo','grsWt','itmAmount','cash','card','online'];
 
+  rprRclStartDate:Date=new Date();
+  rprRclEndDate:Date=new Date();
+  custList:any=[];
+  cstName:any;
+  reprNo:any;
+  delvryNo:any;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private service: ErpService){
+  }
 }
