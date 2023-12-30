d252508c050cfe772f8db90cc773ce03dc752724
manasa1230 <91188797+manasa1230@users.noreply.github.com>
2023-12-30 10:22:26 +0530

    added Stone-Purchase Register

--- src/app/stone-purchase-register/stone-purchase-register.component.ts
+++ src/app/stone-purchase-register/stone-purchase-register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ErpService } from '../erp.service';
 
 @Component({
   selector: 'app-stone-purchase-register',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./stone-purchase-register.component.css']
 })
 export class StonePurchaseRegisterComponent {
+  displayedColumns: string[] = ['position','voucherNO','date','mainPrdt','prtyName','gwt','less','nwt','totalAmt','cgst','sgst','igst','grsAmt'];
 
+  stnPrcsStrtDt:Date=new Date();
+  stnPrcsEndDt:Date=new Date();
+  prodList:any=[];
+  prodName:any;
+  prtyList:any=[];
+  prtyName:any;
+  invNO:any;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private service: ErpService){
+  }
 }
