254df51681bcbcccf8a21c3f6b9a4e0c05050405
manasa1230 <91188797+manasa1230@users.noreply.github.com>
2023-12-30 09:59:38 +0530

    purchase Return

--- src/app/purchase-return-register/purchase-return-register.component.ts
+++ src/app/purchase-return-register/purchase-return-register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ErpService } from '../erp.service';
 
 @Component({
   selector: 'app-purchase-return-register',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./purchase-return-register.component.css']
 })
 export class PurchaseReturnRegisterComponent {
+  displayedColumns: string[] = ['position','voucherNO','date','mainPrdt','prtyName','gwt','less','nwt','totalAmt','cgst','sgst','igst','grsAmt'];
 
+  prchseStartDate:Date=new Date();
+  prchseEndDate:Date=new Date();
+  prtyList:any=[];
+  partyName:any;
+  prodList:any=[];
+  prodName:any;
+  purtyList:any=[];
+  purtyName:any;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private service: ErpService){
+  }
 }
