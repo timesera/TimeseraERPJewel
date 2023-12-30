9fe925ea198aca084d207ecb5445f4bd83567dd3
manasa1230 <91188797+manasa1230@users.noreply.github.com>
2023-12-30 13:38:16 +0530

    added Repair-Delivery-Register

--- src/app/repair-delivery-register/repair-delivery-register.component.ts
+++ src/app/repair-delivery-register/repair-delivery-register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ErpService } from '../erp.service';
 
 @Component({
   selector: 'app-repair-delivery-register',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./repair-delivery-register.component.css']
 })
 export class RepairDeliveryRegisterComponent {
+  displayedColumns: string[] = ['position','reprNo','date','delNo','delDate','custmrName','mobileNo','grsWt','addWt','actlWt','totalAmt','lessAmt','netAmt'];
 
+  rprDelStartDate:Date=new Date();
+  rprDelEndDate:Date=new Date();
+  custList:any=[];
+  cstName:any;
+  reprNo:any;
+  delvryNo:any;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private service: ErpService){
+  }
 }
