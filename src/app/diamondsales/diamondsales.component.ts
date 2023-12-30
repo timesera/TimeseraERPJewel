c39bcf85ccbb3b804873028503a8278eb4a86d26
manasa1230 <91188797+manasa1230@users.noreply.github.com>
2023-12-30 12:15:55 +0530

    added Diamond Sales

--- src/app/diamondsales/diamondsales.component.ts
+++ src/app/diamondsales/diamondsales.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ErpService } from '../erp.service';
 
 @Component({
   selector: 'app-diamondsales',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./diamondsales.component.css']
 })
 export class DiamondsalesComponent {
+  displayedColumns: string[] = ['position','billNo','billDt','tagNo','prodName','pcs','gwt','nwt','diaCts','diaAmt'];
 
+  dmdSaleStrtDt:Date=new Date();
+  dmdSaleEndDt:Date=new Date();
+  billNo:any;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private service: ErpService){
+  }
 }
