5335c640973c0d2fe9ad54f141445acec1b1bd39
timesera <timesera9090@gmail.com>
2024-01-20 19:00:51 +0530

    Day transcation changes

--- src/app/day-transcations/day-transcations.component.ts
+++ src/app/day-transcations/day-transcations.component.ts
@@ -10,23 +10,79 @@ import { TableUtil } from '../tableUtil';
   templateUrl: './day-transcations.component.html',
   styleUrls: ['./day-transcations.component.css']
 })
-export class DayTranscationsComponent implements OnInit{
-  startDate:Date=new Date();
-  totalCash:number = 0;
-  constructor(private service: ErpService,private datePipe: DatePipe){
+export class DayTranscationsComponent implements OnInit {
+  startDate: Date = new Date();
+  totalCash: number = 0;
+  dayOpening: number = 0;
+  mainHeader: any = "";
+  totalHeader: number = 1;
+  totalHeaderName: any = "";
+  dayTranscationList: any = Array<any>();
+  constructor(private service: ErpService, private datePipe: DatePipe) {
   }
   ngOnInit() {
-
+    this.getDayTranscationDetails();
   }
 
-  getDayTranscationDetails(){
-    this.service.GetDayTranscations('2023/12/20').subscribe(data => {
-      
+  getDayTranscationDetails() {
+    let saleStartingDate = this.datePipe.transform(this.startDate, 'yyyy/MM/dd') || '';
+    this.service.GetDayTranscations(saleStartingDate).subscribe(data => {
+      if(data.dayOpening != undefined){
+        this.dayOpening = Number(data.dayOpening.sumCredit) - Number(data.dayOpening.sumDebit);
+      }
+      this.dayTranscationList = data.dayTransactionsList;
     });
 
   }
+  validateMainHeader(name: any){
+    if(this.mainHeader != name){
+      this.mainHeader = name;
+      return true;
+    }
+    return false;
+  }
+  showTotal(name: any){
+    let count = this.dayTranscationList.filter((re: any) => re.mainhead == name).length;
+    if(count == this.totalHeader && this.totalHeaderName != name){
+      this.totalHeader = 1;
+      this.totalHeaderName = name;
+      return true;
+    }
+    this.totalHeader += 1;
+    return false;
+  }
+  getTotal(title: any, columnName: any){
+    let total = 0;
+    var tempList = this.dayTranscationList.filter((re: any) => re.mainhead == title)
+    tempList.forEach((item: any) => {
+      if(columnName == "MIGWT"){
+        total += Number(item.isS_GWT);
+      }
+      else if(columnName == "MINWT"){
+        total += Number(item.isS_NWT);
+      }
+      else if(columnName == "MRGWT"){
+        total += Number(item.reC_GWT);
+      }
+      else if(columnName == "MRNWT"){
+        total += Number(item.reC_NWT);
+      }
+      else if(columnName == "CGWT"){
+        total += Number(item.crediT_RECEIPT);
+      }
+      else if(columnName == "CNWT"){
+        total += Number(item.debiT_PAYMENT);
+      }
+    });
+  
+    return total;
+  }
 
-  exportDataSource(){
+  exportExcelDataSource() {
+
+  }
+
+  exportPDFDataSource() {
 
   }
 }
