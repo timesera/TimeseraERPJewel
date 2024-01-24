2a26c3f0e2e15b44935b4aadea328f7b385d0b76
timesera <timesera9090@gmail.com>
2024-01-24 15:04:41 +0530

    export to excelchanges

--- src/app/day-glance/day-glance.component.ts
+++ src/app/day-glance/day-glance.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ErpService } from '../erp.service';
+import { DatePipe } from '@angular/common';
+import { TableUtil } from '../tableUtil';
 
 @Component({
   selector: 'app-day-glance',
@@ -7,4 +10,100 @@ import { Component } from '@angular/core';
 })
 export class DayGlanceComponent {
 
+  getDayGlanceDetails() {
+    let startDate = this.datePipe.transform(this.startDate, 'yyyy/MM/dd') || '';
+    let endDate = this.datePipe.transform(this.endDate, 'yyyy/MM/dd') || '';
+    this.service.GetDayGlanceDetails(startDate, endDate).subscribe(data => {
+      this.dayGlanceList = data;
+    });
+
+  }
+  validateMainHeader(name: any){
+    if(this.mainHeader != name){
+      this.mainHeader = name;
+      return true;
+    }
+    return false;
+  }
+  showTotal(name: any){
+    let count = this.dayGlanceList.filter((re: any) => re.transtype == name).length;
+    if(count == this.tableTotalHeader && this.tableTotalHeaderName != name){
+      this.tableTotalHeader = 1;
+      this.tableTotalHeaderName = name;
+      return true;
+    }
+    this.tableTotalHeader += 1;
+    return false;
+  }
+  getTotal(title: any, columnName: any){
+    let total = 0;
+    let oggwt = 0;
+    let ognwt = 0;
+    let ogamt = 0;
+    var tempList = this.dayGlanceList.filter((re: any) => re.transtype == title)
+    tempList.forEach((item: any) => {
+      if(columnName == "GWT"){
+        total += Number(item.gwt);
+      }
+      else if(columnName == "NWT"){
+        total += Number(item.nwt);
+      }
+      else if(columnName == "TOTAMT"){
+        total += Number(item.totamt);
+      }
+      else if(columnName == "CGST"){
+        total += Number(item.cgst);
+      }
+      else if(columnName == "SGST"){
+        total += Number(item.sgst);
+      }
+      else if(columnName == "IGST"){
+        total += Number(item.igst);
+      }
+      else if(columnName == "NETAMT"){
+        total += Number(item.netamt);
+      }
+      else if(columnName == "ODG"){
+        oggwt += Number(item.oggwt);
+        ognwt += Number(item.ognwt);
+        ogamt += Number(item.ogamt);
+      }
+      else if(columnName == "ODS"){
+        oggwt += Number(item.oggwt);
+        ognwt += Number(item.ognwt);
+        ogamt += Number(item.ogamt);
+      }
+      else if(columnName == "ORDADV"){
+        total += Number(item.ordadv);
+      }
+      else if(columnName == "CHE"){
+        total += Number(item.cheque);
+      }
+      else if(columnName == "CARD"){
+        total += Number(item.card);
+      }
+      else if(columnName == "CASH"){
+        total += Number(item.cash);
+      }
+      else if(columnName == "SCH"){
+        total += Number(item.scheme);
+      }
+      else if(columnName == "BAL"){
+        total += Number(item.balance);
+      }
+      else if(columnName == "ONL"){
+        total += Number(item.online);
+      }
+    });
+  
+    return (columnName !="ODS" && columnName !="ODG") ? total : (oggwt +"|"+ ognwt +"|"+ ogamt);
+  }
+
+  exportExcelDataSource() {
+    TableUtil.exportTableToExcel("day-glance-table","Day Glance Report");
+  }
+
+  exportPDFDataSource() {
+
+  }
 }
--- src/app/day-transcations/day-transcations.component.ts
+++ src/app/day-transcations/day-transcations.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ErpService } from '../erp.service';
+import { DatePipe } from '@angular/common';
+import { TableUtil } from '../tableUtil';
 
 @Component({
   selector: 'app-day-transcations',
   templateUrl: './day-transcations.component.html',
   styleUrls: ['./day-transcations.component.css']
 })
-export class DayTranscationsComponent {
+export class DayTranscationsComponent implements OnInit {
 
+  startDate: Date = new Date();
+  totalCashReceipt: number = 0;
+  totalCashPayment: number = 0;
+  cashinHand: number = 0;
+  dayOpening: number = 0;
+  mainHeader: any = "";
+  tableTotalHeader: number = 1;
+  tableTotalHeaderName: any = "";
+  dayTranscationList: any = Array<any>();
+  constructor(private service: ErpService, private datePipe: DatePipe) {
+  }
+  ngOnInit() {
+    this.getDayTranscationDetails();
+  }
+
+  getDayTranscationDetails() {
+    let saleStartingDate = this.datePipe.transform(this.startDate, 'yyyy/MM/dd') || '';
+    this.service.GetDayTranscations(saleStartingDate).subscribe(data => {
+      if(data.dayOpening != undefined){
+        this.dayOpening = Number(data.dayOpening.sumCredit) - Number(data.dayOpening.sumDebit);
+      }
+      this.dayTranscationList = data.dayTransactionsList;
+      this.totalCashReceipt = this.dayTranscationList.reduce((a: any, b: { crediT_RECEIPT: any; }) => a + b.crediT_RECEIPT, 0);
+      this.totalCashPayment = this.dayTranscationList.reduce((a: any, b: { debiT_PAYMENT: any; }) => a + b.debiT_PAYMENT, 0);
+      this.cashinHand = ((this.dayOpening + this.totalCashReceipt) - this.totalCashPayment);
+    });
+
+  }
+  validateMainHeader(name: any){
+    if(this.mainHeader != name){
+      this.mainHeader = name;
+      return true;
+    }
+    return false;
+  }
+  showTotal(name: any){
+    let count = this.dayTranscationList.filter((re: any) => re.mainhead == name).length;
+    if(count == this.tableTotalHeader && this.tableTotalHeaderName != name){
+      this.tableTotalHeader = 1;
+      this.tableTotalHeaderName = name;
+      return true;
+    }
+    this.tableTotalHeader += 1;
+    return false;
+  }
+  getTotal(title: any, columnName: any){
+    let total = 0;
+    var tempList = this.dayTranscationList.filter((re: any) => re.mainhead == title)
+    tempList.forEach((item: any) => {
+      if(columnName == "MIGWT"){
+        total += Number(item.isS_GWT);
+      }
+      else if(columnName == "MINWT"){
+        total += Number(item.isS_NWT);
+      }
+      else if(columnName == "MRGWT"){
+        total += Number(item.reC_GWT);
+      }
+      else if(columnName == "MRNWT"){
+        total += Number(item.reC_NWT);
+      }
+      else if(columnName == "CGWT"){
+        total += Number(item.crediT_RECEIPT);
+      }
+      else if(columnName == "CNWT"){
+        total += Number(item.debiT_PAYMENT);
+      }
+    });
+  
+    return total;
+  }
+
+  exportExcelDataSource() {
+    TableUtil.exportTableToExcel("day-transcation-table","Day Transcation Report");
+  }
+
+  exportPDFDataSource() {
+
+  }
 }
